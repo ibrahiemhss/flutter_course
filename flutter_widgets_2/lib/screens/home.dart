1218f9d1b764968e86247f3b2f08ca66cf66746a
ibrahiemhss <kekoreko3@gmail.com>
2019-02-24 01:24:02 +0200

    margin , padding and box constraints && adding custom properties for text

--- flutter_widgets_2/lib/screens/home.dart
+++ flutter_widgets_2/lib/screens/home.dart
@@ -9,14 +9,27 @@ class Home extends StatelessWidget{
 
           alignment: Alignment.center,
           color: Colors.deepPurple,
-      //    width: 200.0,
-      //    height: 100.0,
-      //  margin: EdgeInsets.all(50.0),
+      ////-----margin , padding and box constraints-----------
+        /*width: 200.0,
+        height: 100.0,
+        margin: EdgeInsets.all(50.0),
         margin: EdgeInsets.only(left:50.0,right: 50.0,top: 10.0),
-       // padding: EdgeInsets.all(50.0),
+        padding: EdgeInsets.all(50.0),*/
 
-        child: Text("Flight",textDirection: TextDirection.ltr),
-      ),
+        child: Text("Flight",
+            textDirection: TextDirection.ltr,
+          ///----adding custom properties for text----------
+          style: TextStyle(
+            //line under text
+            decoration: TextDecoration.none,
+              fontSize: 100.0,
+              fontFamily: 'Muli',
+              fontWeight: FontWeight.w700,
+              fontStyle: FontStyle.italic,
+              color: Colors.white
+          ),
+        ),
+      )
     );
   }
 

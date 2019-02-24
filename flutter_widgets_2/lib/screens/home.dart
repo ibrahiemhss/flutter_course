0a46a157e3e400a1d60c85ca6d977eebcd4b80b0
ibrahiemhss <kekoreko3@gmail.com>
2019-02-24 12:12:31 +0200

    AlertDialog

--- flutter_widgets_2/lib/screens/home.dart
+++ flutter_widgets_2/lib/screens/home.dart
@@ -1,174 +1,170 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      child: Container(
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 50.0),
 
-          alignment: Alignment.center,
-          color: Colors.deepPurple,
-          padding: EdgeInsets.only(left:10.0,right: 10.0,top: 50.0),
-
-          ////-----margin , padding and box constraints-----------
-        /*width: 200.0,
+            ////-----margin , padding and box constraints-----------
+            /*width: 200.0,
         height: 100.0,
         margin: EdgeInsets.all(50.0),
         padding: EdgeInsets.all(50.0),*/
 
-        child:Column(
-          children: <Widget>[
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Flight To ",
+                      textDirection: TextDirection.ltr,
 
-         Row(
-        children: <Widget>[
+                      ///----adding custom properties for text----------
+                      style: TextStyle(
+                          //line under text
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          fontFamily: 'Muli',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Sinai  ",
+                      textDirection: TextDirection.ltr,
 
-          Expanded(child: Text("my name is  ",
-          textDirection: TextDirection.ltr,
+                      ///----adding custom properties for text----------
+                      style: TextStyle(
+                          //line under text
+                          decoration: TextDecoration.none,
+                          fontSize: 10.0,
+                          fontFamily: 'Muli',
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "ate  24/2/2019",
+                      textDirection: TextDirection.ltr,
 
-          ///----adding custom properties for text----------
-          style: TextStyle(
-            //line under text
-              decoration: TextDecoration.none,
-              fontSize: 20.0,
-              fontFamily: 'Muli',
-              fontWeight: FontWeight.w700,
-              fontStyle: FontStyle.italic,
-              color: Colors.white),
-        )),
-          Expanded(child: Text("ibrahim  ",
-        textDirection: TextDirection.ltr,
+                      ///----adding custom properties for text----------
+                      style: TextStyle(
+                          //line under text
+                          decoration: TextDecoration.none,
+                          fontSize: 10.0,
+                          fontFamily: 'Muli',
+                          color: Colors.white),
+                    ))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "column2 row1 ",
+                      textDirection: TextDirection.ltr,
 
-        ///----adding custom properties for text----------
-        style: TextStyle(
-          //line under text
-            decoration: TextDecoration.none,
-            fontSize: 10.0,
-            fontFamily: 'Muli',
-            color: Colors.white),
-      )),
-          Expanded(child: Text("age  22 y ",
-        textDirection: TextDirection.ltr,
+                      ///----adding custom properties for text----------
+                      style: TextStyle(
+                          //line under text
+                          decoration: TextDecoration.none,
+                          fontSize: 10.0,
+                          fontFamily: 'Muli',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "column2 row2 ",
+                      textDirection: TextDirection.ltr,
 
-        ///----adding custom properties for text----------
-        style: TextStyle(
-          //line under text
-            decoration: TextDecoration.none,
-            fontSize: 10.0,
-            fontFamily: 'Muli',
-            color: Colors.white),
-      ))
-      ],
-    ),
+                      ///----adding custom properties for text----------
+                      style: TextStyle(
+                          //line under text
+                          decoration: TextDecoration.none,
+                          fontSize: 10.0,
+                          fontFamily: 'Muli',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "column2 row3 ",
+                      textDirection: TextDirection.ltr,
 
-
-         Row(
-           children: <Widget>[
-
-             Expanded(child: Text("column2 row1 ",
-               textDirection: TextDirection.ltr,
-
-               ///----adding custom properties for text----------
-               style: TextStyle(
-                 //line under text
-                   decoration: TextDecoration.none,
-                   fontSize: 10.0,
-                   fontFamily: 'Muli',
-                   fontWeight: FontWeight.w700,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white),
-             )),
-             Expanded(child: Text("column2 row2 ",
-               textDirection: TextDirection.ltr,
-
-               ///----adding custom properties for text----------
-               style: TextStyle(
-                 //line under text
-                   decoration: TextDecoration.none,
-                   fontSize: 10.0,
-                   fontFamily: 'Muli',
-                   fontWeight: FontWeight.w700,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white),
-             )),
-             Expanded(child: Text("column2 row3 ",
-               textDirection: TextDirection.ltr,
-
-               ///----adding custom properties for text----------
-               style: TextStyle(
-                 //line under text
-                   decoration: TextDecoration.none,
-                   fontSize: 10.0,
-                   fontFamily: 'Muli',
-                   fontWeight: FontWeight.w700,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white),
-             ))
-           ],
-         ),
-
-         Row(
-           children: <Widget>[
-
-             Expanded(child: Text("column3 row1 ",
-               textDirection: TextDirection.ltr,
-
-               ///----adding custom properties for text----------
-               style: TextStyle(
-                 //line under text
-                   decoration: TextDecoration.none,
-                   fontSize: 10.0,
-                   fontFamily: 'Muli',
-                   fontWeight: FontWeight.w700,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white),
-             )),
-             Expanded(child: Text("column3 row2 ",
-               textDirection: TextDirection.ltr,
-
-               ///----adding custom properties for text----------
-               style: TextStyle(
-                 //line under text
-                   decoration: TextDecoration.none,
-                   fontSize: 10.0,
-                   fontFamily: 'Muli',
-                   fontWeight: FontWeight.w700,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white),
-             )),
-             Expanded(child: Text("column3 row3 ",
-               textDirection: TextDirection.ltr,
-
-               ///----adding custom properties for text----------
-               style: TextStyle(
-                 //line under text
-                   decoration: TextDecoration.none,
-                   fontSize: 10.0,
-                   fontFamily: 'Muli',
-                   fontWeight: FontWeight.w700,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white),
-             ))
-           ],
-         ),
-
-            FlightImageAsset()
-          ],
-        )
-
-      )
-    );
+                      ///----adding custom properties for text----------
+                      style: TextStyle(
+                          //line under text
+                          decoration: TextDecoration.none,
+                          fontSize: 10.0,
+                          fontFamily: 'Muli',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    ))
+                  ],
+                ),
+                FlightImageAsset(),
+                FlightBookButton()
+              ],
+            )));
   }
-
 }
-class FlightImageAsset extends StatelessWidget{
+
+class FlightImageAsset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    AssetImage assetImage=AssetImage("images/offers.png");
-    Image image=Image(image: assetImage,width: 250.0,height: 250.0,);
+    AssetImage assetImage = AssetImage("images/flight.png");
+    Image image = Image(
+      image: assetImage,
+      width: 250.0,
+      height: 250.0,
+    );
 
-    return Container(child: image,);
+    return Container(
+      child: image,
+    );
+  }
+}
+
+class FlightBookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.only(top: 40.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.orange,
+          child: Text(
+            "book your flight",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: "Muli",
+                fontWeight: FontWeight.w700),
+          ),
+          elevation: 6.0,
+          onPressed: () => bookFlight(context)),
+    );
   }
 
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight Booked successfully"),
+      content: Text("Have a pleasant flight"),
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
+  }
 }

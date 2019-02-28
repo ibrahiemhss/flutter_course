f9ebfe609caccaccb04cf94efa528090d7890989
ibrahiemhss <kekoreko3@gmail.com>
2019-03-01 00:46:45 +0200

    add image , column items and row items Widgets

--- calculator_app_5/lib/main.dart
+++ calculator_app_5/lib/main.dart
@@ -1,17 +1,106 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
 
-class MyApp extends StatelessWidget {
+  runApp(
+      MaterialApp(
+        title: "Simple Calculator App",
+        home: SIForm (),
+      )
+  );
+}
+
+class SIForm extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-    );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SIFormState();
   }
 }
 
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Liver', 'Dinar', 'Pounds'];
+  final _minimumPadding=5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple Calculator App"),
+      ),
+      body: Container(
+          margin:EdgeInsets.all(_minimumPadding * 10),
+        child: Column(
+          children: <Widget>[
+            getImageAsset(),
+
+            Padding(
+              padding: EdgeInsets.only(top: _minimumPadding,bottom: _minimumPadding),
+              child:
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'principal',
+                hintText: 'Enter princibal e.g 12000',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0)
+                )
+              ) ,
+            ),),
+        Padding(
+          padding: EdgeInsets.only(top: _minimumPadding,bottom: _minimumPadding),
+          child:
+        TextField(
+              decoration: InputDecoration(
+                  labelText: 'Rate of interest',
+                  hintText: 'In percent',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)
+                  )
+              ) ,
+        ),),
+
+            Row(
+              children: <Widget>[
+                Expanded (
+                    child: TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Term',
+                      hintText: 'Time in year',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0)
+                      )
+                  ) ,
+                )),
+
+                Expanded (child: DropdownButton<String>(
+
+                  items: _currencies.map((String value){
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+
+                  }).toList(),
+                 // value :'Livers',
+                  onChanged: (String newValueSelcted){
+
+                  },
+                ))
+
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  Widget getImageAsset(){
+    AssetImage assetImage=AssetImage('images/money.png');
+    Image image=Image(image: assetImage,width: 125.0,height: 125.0,);
+    return Container(
+      child:
+      image,);
+  }
+}
--- calculator_app_5/test/widget_test.dart
+++ calculator_app_5/test/widget_test.dart
@@ -5,46 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calculator_app_5/main.dart';
 
-void main() {
 
-  runApp(
-    MaterialApp(
-      title: "Simple Calculator App",
-      home: SIForm (),
-    )
-  );
-}
-
-class SIForm extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _SIFormState();
-  }
-}
-
-class _SIFormState extends State<SIForm>{
-  var _currencies=['Liver','Dinar','Pounds'];
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Simple Calculator App"),
-      ),
-      body:
-      Container(
-        child: Column(
-          children: <Widget>[
-
-          ],
-        ),
-      ),
-    );
-  }
-}

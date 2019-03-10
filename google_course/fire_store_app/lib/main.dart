572668466f7083a001850291c99f455726612c66
ibrahiemhss <kekoreko3@gmail.com>
2019-03-10 20:13:50 +0200

    get data functions from firestore

--- google_course/fire_store_app/lib/main.dart
+++ google_course/fire_store_app/lib/main.dart
@@ -1,111 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Possible Band Names'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MockBandInfo {
+  const MockBandInfo({this.name, this.votes});
+
+  final String name;
+  final int votes;
+}
+
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  Widget _builderListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: [
+          Expanded(
+            child: Text(
+              //Firestore.instance.collection('bandanames').document('name').get(),
+              document['name'],
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              color: Color(0xffddddff),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              document['votes'].toString(),
+              style: Theme.of(context).textTheme.display1,
+            ),
+          )
+        ],
+      ),
+      onTap: () {
+        //First way
+        /*document.reference.updateData({
+          "votes":document['votes']+1
+        });*/
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+        //Second way
+        Firestore.instance.runTransaction((transaction) async{
+          DocumentSnapshot freshSnap=
+              await transaction.get(document.reference);
+          await transaction.update(freshSnap.reference,{
+            "votes":document['votes']+1,
+          });
+        });
+      },
+    );
   }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    // TODO: implement build
+     return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: StreamBuilder(
+            stream:Firestore.instance.collection('bandanames').snapshots(),
+            builder:(context,AsyncSnapshot<QuerySnapshot> snapshot)
+            {
+              if (snapshot.hasError) {
+                print(snapshot.error.toString());
+              }else if(!snapshot.hasData) {
+                return const Text('Loaing...');
+
+              }else {
+                return ListView.builder(
+                  itemExtent: 80.0,
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) =>
+                      _builderListItem(context, snapshot.data.documents[index]),
+                );
+              }
+            }
+          ),
+
+            ///way 2
+           /* child: StreamBuilder(
+                stream:Firestore.instance.collection('bandanames').snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot>snapshot) {
+                  if (snapshot.hasError)
+                    return new Text(
+                        'Error: ${snapshot.error}');
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return new Text('Loading...');
+                    default:
+                      return new ListView(
+                        children: snapshot.data.documents.map((
+                            DocumentSnapshot document) {
+                          return new ListTile(
+                            title: new Text(document['name']),
+                            subtitle: new Text(document['votes'].toString()),
+                          );
+                        }).toList(),
+                      );
+                  }
+                }
+
+            )*/
+        )
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }

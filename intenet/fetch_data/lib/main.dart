1413a03ab42bc3656be619b7d0a6fd1c1ee8d7bc
ibrahiemhss <kekoreko3@gmail.com>
2019-03-13 22:27:50 +0200

    cryptoApp

--- intenet/fetch_data/lib/main.dart
+++ intenet/fetch_data/lib/main.dart
@@ -1,12 +1,7 @@
-import 'dart:io';
 
-import 'package:http/http.dart';
-
-import 'package:fetch_data/model/model.dart';
 import "package:flutter/material.dart";
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' show post;
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(
